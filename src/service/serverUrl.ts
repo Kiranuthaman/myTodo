e79eb47ce677ddbed23ba8d59cc7b7aa4f700a64
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-16 19:13:12 +0530

    server url changed

--- src/service/serverUrl.ts
+++ src/service/serverUrl.ts
@@ -1,1 +1,2 @@
-export const serverUrl: string = "http://localhost:5000";
+// export const serverUrl: string = "http://localhost:5000";
+export const serverUrl: string = "https://todo-server-4hs.onrender.com";
