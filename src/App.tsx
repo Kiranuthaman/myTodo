ca727d32769382ebfcce7b081a8fc42bb6e71fd0
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-16 13:43:59 +0530

    proper routing completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Auth from './pages/Auth'
-import Dashboard from './pages/Dashboard'
-import AllTodo from './layouts/AllTodo'
-import OnGoing from './layouts/OnGoing'
-import Completed from './layouts/Completed'
-import UserProfie from './layouts/UserProfie'
-import Favorate from './layouts/Favorate'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import AllTodo from "./layouts/AllTodo";
+import OnGoing from "./layouts/OnGoing";
+import Completed from "./layouts/Completed";
+import UserProfie from "./layouts/UserProfie";
+import Favorate from "./layouts/Favorate";
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/register" element={<Auth register = {true}/>} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route index element={<OnGoing />} />
-          <Route index path='alltasks' element={<AllTodo />} />
-          <Route path="completed" element={<Completed />} />
-          <Route path="favorate" element={<Favorate />} />
-          <Route path="profile" element={<UserProfie />} />
+        {/* Public Routes */}
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<Auth />} />
+          <Route path="/register" element={<Auth register={true} />} />
         </Route>
-      </Routes>
 
+        {/* Protected Routes */}
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route element={<Dashboard />}>
+            <Route index element={<OnGoing />} />
+            <Route path="alltasks" element={<AllTodo />} />
+            <Route path="completed" element={<Completed />} />
+            <Route path="favorate" element={<Favorate />} />
+            <Route path="profile" element={<UserProfie />} />
+          </Route>
+        </Route>
+
+        {/* Catch-All */}
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
